90d950ad900678b68a2e38eb3b8446e6596d5e95
cookie0215 <jina_0215@naver.com>
2021-11-23 17:23:17 +0900

    [21.11.23] 15의 배수 fizzbuzz출력

--- main.js
+++ main.js
@@ -12,9 +12,12 @@ forEach로 작성하기
 */
 
 for(let i=1; i<=100; i++){
-    if( i % 3 === 0 ) {
+    if( (i % 3 === 0) && (i % 5 === 0) ) {
+        console.log(`숫자${i}는 fizzbuzz`);
+    }else if( i % 3 === 0 ) {
         console.log(`숫자${i}는 fizz`);
     }else if( i % 5 === 0 ) {
         console.log(`숫자${i}는 buzz`);
     }
+    
 }
