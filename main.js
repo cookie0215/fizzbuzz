fb15b1ead5b1400290aa24d7d87a9ba34794015c
cookie0215 <jina_0215@naver.com>
2021-11-23 16:18:11 +0900

    [21.11.13] 5의 배수 buzz 출력

--- main.js
+++ main.js
@@ -14,5 +14,7 @@ forEach로 작성하기
 for(let i=1; i<=100; i++){
     if( i % 3 === 0 ) {
         console.log(`숫자${i}는 fizz`);
+    }else if( i % 5 === 0 ) {
+        console.log(`숫자${i}는 buzz`);
     }
 }
